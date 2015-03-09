b3dcefc261b9c570f054f346ea9df574d962d653
yiyanggeotel <yiyang@geo-tel.com>
2015-03-09 16:51:39 -0400

    ajax

--- test.php
+++ test.php
@@ -2,38 +2,26 @@
 <html>
 <head>
 <title>Title of the document</title>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+<script>
+$(document).ready(function(){
+	$.ajax({
+	  url: "testmysql.php",
+	})
+  	.done(function( data ) {
+	    alert(data);
+	});
+ });
+
+</script>
+
 </head>
 
 <body>
 The content of the document......
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "password";
-$dbname="test";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
-
-$sql = "SELECT * from csvtest";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "InvestID: " . $row["InvestID"]. " - Investment: " . $row["Investment"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-
-?>
 </body>
 
 </html>
--- testmysql.php
+++ testmysql.php
@@ -1,7 +1,47 @@
-<?php 
-$link = mysql_connect('hostname','dbuser','dbpassword'); 
-if (!$link) { 
-	die('Could not connect to MySQL: ' . mysql_error()); 
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "password";
+$dbname="test";
+
+header("Content-type: text/xml");
+
+$dom = new DOMDocument("1.0");
+$node = $dom->createElement("markers");
+$parnode = $dom->appendChild($node);
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 } 
-echo 'Connection OK'; mysql_close($link); 
-?> 
+//echo "Connected successfully<br/>";
+
+$sql = "SELECT * from testtable";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        //echo "address: " . $row["address"]. " - coordination: " . $row["lat"] . "," . $row["lon"] . "<br>";
+
+        $node = $dom->createElement("marker");
+	  $newnode = $parnode->appendChild($node);
+	  $newnode->setAttribute("name",$row['name']);
+	  $newnode->setAttribute("address", $row['address']);
+	  $newnode->setAttribute("lat", $row['lat']);
+	  $newnode->setAttribute("lng", $row['lon']);
+	  $newnode->setAttribute("type", $row['type']);
+    }
+} else {
+    //echo "0 results";
+}
+
+//var_dump($dom);
+echo $dom->saveXML();
+
+//echo "<id>1</id>";
+
+?>
