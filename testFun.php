aa1e0a8a9b7660a1de52555aecf38888202e387d
yiyanggeotel <yiyang@geo-tel.com>
2015-03-11 16:46:39 -0400

    amazon

--- testFun.php
+++ testFun.php
@@ -2,12 +2,5 @@
 
 <?php
 
-$dom = new DOMDocument('1.0', 'utf-8');
-
-$element = $dom->createElement('test', 'This is the root element!');
-
-// We insert the new element as root (child of the document)
-$dom->appendChild($element);
-
-echo $dom->saveXML();
+phpinfo();
 ?>
