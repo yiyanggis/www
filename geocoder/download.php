d494c33104517ebe03a39dc77a19702861c7630c
yiyanggeotel <yiyang@geo-tel.com>
2015-04-08 12:33:21 -0400

    fix geocoder download error

--- geocoder/download.php
+++ geocoder/download.php
@@ -4,9 +4,9 @@
 
 header("Cache-Control: public");
 header("Content-Description: File Transfer");
-header("Content-Disposition: attachment; filename= test.csv");
+header("Content-Disposition: attachment; filename= result.csv");
 header("Content-Transfer-Encoding: binary");    
-readfile("csv/test.csv");
+readfile("result/file.csv");
 
 
 ?>
