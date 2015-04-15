9b450861790bcbdbe713ddc544e927a3f3020ecc
yiyanggeotel <yiyang@geo-tel.com>
2015-04-15 16:43:53 -0400

    test post

---
+++ test/test.php
@@ -0,0 +1,17 @@
+<?php
+
+header("content-type:application/json");
+
+print_r($_POST);
+
+var_dump($_POST);
+
+$path=$_POST["path"];
+#$lat=floatval($_POST["lat"]);
+#$lon=floatval($_POST["lon"]);
+#$desc=$_POST["desc"];
+
+echo "{'test':'value'}"
+
+
+?>
