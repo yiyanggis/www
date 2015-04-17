8add86ab97ae3cebe926cb266ddbb5b092eef1ee
yiyanggeotel <yiyang@geo-tel.com>
2015-04-17 13:38:05 -0400

    right panel

---
+++ photo/js/imageFunctions.js
@@ -0,0 +1,26 @@
+var FlagImage=function(url, marker, date, desc){
+	this.imageURL=url;
+	this.marker=marker;
+	this.date=date;
+	this.desc=desc;
+}
+
+FlagImage.prototype.focusOn=function(){
+	if(this.marker!=undefined){
+		if(this.marker.map!=undefined){
+			var map=this.marker.map;
+			 map.setCenter(this.marker.getPosition());
+			 new google.maps.event.trigger( this.marker, 'click' );
+
+			 return true;
+		}
+		else{
+			return false;
+		}
+	}
+	else{
+		return false;
+	}
+	
+
+}
